eb8ed2c06fc8f1ba39fdfd993cd79c875d8354e4
ssamkkim <ssamuel.kkim@gmail.com>
2022-07-07 17:28:23 -0700

    Deleted create-react-app base files and created a basic textarea element

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    console.log(event.target.value);
+    this.setState({
+      input: event.target.value
+    });
+  }
+  render() {
+    return (
+      <div>
+        <textarea id="editor" onChange={this.handleChange} />
+        <p id="preview">{this.state.input}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
