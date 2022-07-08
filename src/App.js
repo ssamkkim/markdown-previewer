5c3516dcb803681d4bfb3d3d7c2ae2d221560ff0
ssamkkim <ssamuel.kkim@gmail.com>
2022-07-08 03:33:21 -0700

    Styled the editor and preview slightly and completed + included the rest of the default features of the editor

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: '# Welcome to my React Markdown Previewer!\n## This is a sub-heading...\n[links](https://www.google.com)\n\n```<div></div>```\n1. List Item 1\n1. List Item 2\n1. List Item 3\n> Block Quote\n\n**Bold Text**\n![Google Logo](https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png)'
+      input: '# Welcome to my React Markdown Previewer!\n## This is a sub-heading\n[Google Link](https://www.google.com)\n\nInline Code, `<div></div>`\n```\n// multi line code block\n\nfunction isEven(num) {\n\tif (even % 2 === 0) {\n\t\treturn true;\n\t} else {\n\t\treturn false;\n\t}\n}\n```\n1. List Item 1\n1. List Item 2\n1. List Item 3\n> Block Quote\n\n**Bold Text**\n![Google Logo](https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png)'
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -17,12 +17,16 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div className="container">
         <textarea id="editor" value={this.state.input} onChange={this.handleChange} />
         <div id="preview" dangerouslySetInnerHTML={{__html: marked.parse(this.state.input)}}></div>
       </div>
     );
   }
 }
+
+marked.setOptions({
+  breaks: true,
+});
 
 export default App;
